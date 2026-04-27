5e35e9a28f6ee6d0d893d59e2fb7a74a43826e2e
yhg1024 <dgr1024kr@naver.com>
2026-04-27 20:24:01 +0900

    Q1 3의 배수도 같이 판별

--- src/Day1.java
+++ src/Day1.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Main {
+public class Day1 {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
@@ -8,8 +8,8 @@ public class Main {
         System.out.println("숫자를 입력하세요: ");
         int number = sc.nextInt();
 
-        if (number % 2 == 0) {
-            System.out.println("짝수입니다.");
+        if (number % 2 == 0 && number % 3 == 0) {
+            System.out.println("짝수이면서 3의 배수입니다.");
         } else {
             System.out.println("홀수입니다.");
         }
