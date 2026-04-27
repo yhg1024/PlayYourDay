8f90742b5899910a14207108a5504c4fa3c140cd
yhg1024 <dgr1024kr@naver.com>
2026-04-27 20:42:25 +0900

    가장 큰 수 출력하기

--- src/Day1.java
+++ src/Day1.java
@@ -5,6 +5,23 @@ public class Day1 {
 
         Scanner sc = new Scanner(System.in);
 
+        // 숫자 3개 입력받기
+        // 가장 큰 수 출력하기
+        System.out.println("첫번째 숫자: ");
+        int a = sc.nextInt();
+        System.out.println("두번째 숫자: ");
+        int b = sc.nextInt();
+        System.out.println("세번째 숫자: ");
+        int c = sc.nextInt();
+
+        if (a >= b &&  a >= c) {
+            System.out.println("첫번째 숫자가 가장 큽니다.");
+        } else if (b >= a && b >= c) {
+            System.out.println("두번째 숫자가 가장 큽니다.");
+        } else {
+            System.out.println("세번째 숫자가 가장 큽니다.");
+        }
+
         // Q2 숫자 2개 입력받기
         System.out.println("첫 번째 숫자: ");
         int number1 = sc.nextInt();
@@ -29,5 +46,9 @@ public class Day1 {
         } else {
             System.out.println("홀수입니다.");
         }
+    }
+
+    private static Scanner getSc(Scanner sc) {
+        return sc;
     }
 }
