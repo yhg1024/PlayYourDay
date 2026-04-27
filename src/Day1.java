51d093b630b7b5aacc4389bdcd037cf4c00c8f7a
yhg1024 <dgr1024kr@naver.com>
2026-04-27 20:32:43 +0900

    Q2 숫자 2개 입력받기

--- src/Day1.java
+++ src/Day1.java
@@ -5,7 +5,23 @@ public class Day1 {
 
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("숫자를 입력하세요: ");
+        // Q2 숫자 2개 입력받기
+        System.out.println("첫 번째 숫자: ");
+        int number1 = sc.nextInt();
+
+        System.out.println("두 번째 숫자:");
+        int number2 = sc.nextInt();
+
+        if (number1 > number2) {
+            System.out.println("더 큰 수: " + number1);
+        } else if (number1 < number2){
+            System.out.println("더 큰 수: " + number2);
+        } else {
+            System.out.println("두 수가 같습니다.");
+        }
+
+
+        // Q1 3의 배수도 같이 판별
         int number = sc.nextInt();
 
         if (number % 2 == 0 && number % 3 == 0) {
