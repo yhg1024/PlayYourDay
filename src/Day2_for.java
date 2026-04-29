05d5e9b83e70fb01307bbbbd5a22ac74eace7ee1
yhg1024 <dgr1024kr@naver.com>
2026-04-29 19:42:36 +0900

    Q3 1부터 10까지 합 구하기

--- src/Day2_for.java
+++ src/Day2_for.java
@@ -3,12 +3,19 @@ public class Day2_for {
         //같은 작업을 여러 번 자동으로 돌리기
         //“코드 줄이는 감각” 익히기
 
-        //Q2 1부터 10까지 출력
+        // Q3 1부터 10까지 합 구하기
+        int sum = 0;
+        for (int i = 1; i <= 10; i++) {
+            sum += i;
+        }
+        System.out.println(sum);
+
+        // Q2 1부터 10까지 출력
         for (int i = 1; i <= 10; i++) {
             System.out.println(i);
         }
 
-        //Q1 “Hello” 5번 출력하기
+        // Q1 “Hello” 5번 출력하기
         for (int i = 0; i < 5; i++) {
             System.out.println("Hello");
         }
