cdc49b9a35c9d89b98f3d838869e2c6886550a2a
yhg1024 <dgr1024kr@naver.com>
2026-04-29 19:38:05 +0900

    Q1 “Hello” 5번 출력하기

---
+++ src/Day2_for.java
@@ -0,0 +1,11 @@
+public class Day2_for {
+    public static void main(String[] args) {
+        //같은 작업을 여러 번 자동으로 돌리기
+        //“코드 줄이는 감각” 익히기
+
+        //Q1 “Hello” 5번 출력하기
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Hello");
+        }
+    }
+}
