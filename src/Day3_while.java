d0c54e15691fec91130d36a2f1cd6bf04d9597ad
yhg1024 <dgr1024kr@naver.com>
2026-05-01 14:47:50 +0900

    랜덤 숫자 맞추기

---
+++ src/Day3_while.java
@@ -0,0 +1,57 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class Day3_while {
+    public static void main(String[] args) {
+        //컴퓨터가 1~100 숫자 하나 정함
+        //내가 계속 숫자 입력
+        //맞출 때까지 반복
+        //정답보다 크면 → “DOWN”
+        //작으면 → “UP”
+        //시도 횟수 출력
+        Scanner sc = new Scanner(System.in);
+        Random random = new Random();
+
+        int answer = random.nextInt(100)+1;
+        int input = 0;
+        int count = 0;
+
+        while (input != answer) {
+            System.out.print("숫자를 입력하세요 : ");
+            input = sc.nextInt();
+            count++;
+
+            if (input > answer) {
+                System.out.println("DOWN");
+            } else if (input < answer) {
+                System.out.println("UP");
+            } else {
+                System.out.println("정답입니다! (" + count + "번 만에 성공)");
+            }
+        }
+
+
+        // 가운데 정렬
+        // 별 1-3-5-7-9 증가
+        for (int i = 1; i <= 5; i++) { // 5줄
+            for (int j = 0; j < 5-i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= i*2-1; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        // 오른쪽 별찍기 복습
+        for (int i = 1; i <= 5; i++) { // 5줄
+            for (int j = 1; j <= 5-i; j++) { // 공백 4-3-2-1-0
+                System.out.print(" ");
+            }
+            for (int j = 1; j < i+1; j++) { // 별 1-2-3-4-5
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
