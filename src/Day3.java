c75a89bac21f57cf2fb4383f2e76196117f314ca
yhg1024 <dgr1024kr@naver.com>
2026-05-01 13:58:19 +0900

    가운데 정렬

--- src/Day3.java
+++ src/Day3.java
@@ -1,5 +1,17 @@
 public class Day3 {
     public static void main(String[] args) {
+        // 가운데 정렬
+        // 별 1-3-5-7-9 증가
+        for (int i = 1; i <= 5; i++) { // 5줄
+            for (int j = 0; j < 5-i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= i*2-1; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
         // 오른쪽 별찍기 복습
         for (int i = 1; i <= 5; i++) { // 5줄
             for (int j = 1; j <= 5-i; j++) { // 공백 4-3-2-1-0
