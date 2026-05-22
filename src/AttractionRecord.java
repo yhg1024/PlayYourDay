58e87acc60346d2d38a33d67dd47375f5c5ab981
yhg1024 <dgr1024kr@naver.com>
2026-05-22 21:21:35 +0900

    롯데월드 기록 콘솔앱 0.1

--- src/AttractionRecord.java
+++ src/AttractionRecord.java
@@ -7,6 +7,8 @@ public class AttractionRecord {
         Scanner sc = new Scanner(System.in);
         List<Attraction> attractions = new ArrayList<>();
 
+        List<RideRecord> records = new ArrayList<>();
+
         //Attraction a1 = new Attraction();
         //a1.id = 1;
         //a1.name = "아틀란티스";
@@ -16,20 +18,68 @@ public class AttractionRecord {
         attractions.add(new Attraction(3,"자이로드롭"));
         attractions.add(new Attraction(4,"자이로스윙"));
 
-        for(Attraction attraction : attractions) {
-            System.out.println(attraction.id + ". " + attraction.name);
+        while(true) {
+            System.out.println("1. 기록 추가");
+            System.out.println("2. 기록 보기");
+            System.out.println("3. 기록 삭제");
+            System.out.println("4. 종료");
+            System.out.print("선택: ");
+            int input = sc.nextInt();
+
+            if (input == 4) break;
+
+            if (input >= 1 && input <= 3)   {
+                if (input == 1) {
+                    //기록 추가
+                    for(Attraction attraction : attractions) {
+                        System.out.println(attraction.id + ". " + attraction.name);
+                    }
+
+                    System.out.print("선택: ");
+                    int choice = sc.nextInt();
+                    //selected 변수 안에
+                    //Attraction 객체 저장
+                    Attraction selected = attractions.get(choice - 1);
+
+                    System.out.println("방문하신 날짜를 입력하세요.");
+                    String date = sc.next();
+
+                    System.out.println("대기시간을 입력하세요.");
+                    int waitTime = sc.nextInt();
+                    RideRecord record = new RideRecord();
+                    record.attractionName = selected.name;
+                    record.date = date;
+                    record.waitTime = waitTime;
+
+                    records.add(record);
+                } else if (input == 2) {
+                    // 기록 보기
+                    for (RideRecord record : records){
+                        System.out.println(record.attractionName + " / " + record.waitTime + "분 / " + record.date);
+                    }
+                } else if (input == 3) {
+                    // 기록 삭제
+                    if (records.isEmpty()) {
+                        System.out.println("기록이 없습니다.");
+                    } else {
+                        for (int i = 0; i < records.size(); i++) {
+                            RideRecord record = records.get(i);
+
+                            System.out.println(
+                                    (i + 1) + ". " +
+                                            record.attractionName + " / " +
+                                            record.waitTime + "분 / " +
+                                            record.date);
+                        }
+                        System.out.println("삭제할 기록을 고르세요.");
+                        int choice = sc.nextInt();
+                        if (choice >= 1 && choice <= records.size()) {
+                            records.remove(choice - 1);
+                        }
+
+                    }
+                }
+            }
         }
-
-        System.out.print("선택: ");
-        int choice = sc.nextInt();
-
-        //selected 변수 안에
-        //Attraction 객체 저장
-        Attraction selected = attractions.get(choice -1);
-
-        RideRecord record = new RideRecord();
-        record.attractionName = selected.name;
-
-        System.out.println(record.attractionName);
     }
 }
--- src/RideRecord.java
+++ src/RideRecord.java
@@ -1,4 +1,5 @@
 public class RideRecord {
+    RideRecord id;
     String attractionName;
     String date;
 
